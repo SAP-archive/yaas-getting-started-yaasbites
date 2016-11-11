f82d73f086ce3d78d786eb28c4a4653b5ef4eafc
KenLomax <ken.lomax@sap.com>
2016-11-11 09:20:26 +0100

    reorganizing teched2016 files

--- yaasbite200/src/main/java/com/hybris/yaas/bites/TipsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.hybris.yaas.bites;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TipsRepo extends CrudRepository<Tip, Long> {
-	
-	
-}
--- yaasbitestorefrontdeltas/two/public/js/app/tips/tips-index.js
+++
@@ -1,6 +0,0 @@
-// ADJUSTED_AS_NEEDED: Added this file which specifies our new Module "ds.tips" where the new Tips Funcionality will live
-
-'use strict';
-angular.module('ds.tips', [
-    'ds.shared' 
-]);

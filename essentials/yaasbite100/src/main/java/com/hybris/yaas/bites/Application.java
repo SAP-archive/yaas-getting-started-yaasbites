68784230f000cd87aba2fb541a0b6231b1db83fb
KenLomax <ken.lomax@hybris.com>
2016-11-14 15:25:46 +0100

    Update Application.java
    Testing demotests

--- essentials/yaasbite100/src/main/java/com/hybris/yaas/bites/Application.java
+++ essentials/yaasbite100/src/main/java/com/hybris/yaas/bites/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {  // See Spring Boot for more
+public class Application {  // See Spring Boot @ https://spring.io/guides/gs/spring-boot/
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

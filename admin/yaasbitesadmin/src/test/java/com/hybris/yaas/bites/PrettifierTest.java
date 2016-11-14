950a8498aa65a53c882e3c685cf7a5fc15f1d36b
KenLomax <ken.lomax@sap.com>
2016-11-14 12:45:15 +0100

    Demotests and cfpush

--- admin/yaasbitesadmin/src/test/java/com/hybris/yaas/bites/PrettifierTest.java
+++ admin/yaasbitesadmin/src/test/java/com/hybris/yaas/bites/PrettifierTest.java
@@ -1,60 +1,94 @@
 package com.hybris.yaas.bites;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
-public class PrettifierTest {	// Using https://code.google.com/p/google-code-prettify/wiki/GettingStarted
-	private static String sourceFiles[] = {
-		"../../essentials/yaasbite100/src/main/java/com/hybris/yaas/bites/GreetingController.java"
+public class PrettifierTest {	// Using https://github.com/google/code-prettify
+	private static String sourceDirectories[] = {
+			"../../essentials/yaasbite100/src/"
 	};
-
+	private static String targetFile = "src/main/webapp/Content.html";
+	
+	private static List<File> listOfFiles = new ArrayList<File>();
+	private static StringBuffer mainHtml = new StringBuffer();
+	private static StringBuffer contentHtml = new StringBuffer();
+	
 	private static String targetDir = "src/main/webapp/demotests";
-	private static String prefix = "<html><head><script src='https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=desert'></script></head><body onload='prettyPrint()'><pre class=prettyprint><code class='language-java '>";
-	private static String postfix = "</code></pre></body></html>";
+	private static String pagePrefix = "<html><head><script src='https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=default'></script><link rel='stylesheet' type='text/css' href='https://www.yaas.io/globalresources/v3/css/global.min.css' media='screen, projection, print'><link rel='stylesheet' href='https://devportal.yaas.io/styles/devportal-yaas.css'></head><body onload='prettyPrint()'><a name='Top'/>";
+	private static String codePrefix = "<a name='%s'/><pre class=prettyprint><code class='language-java '>/* %s */ <a href='#Top'>Top</a>\n";
+	private static String codePostfix = "</code></pre>";
+	private static String pagePostfix = "</body></html>";
 	
 	@Test
 	public void convertJavaToHTML() throws IOException {
-		/*
-		Not running for now - will update next in the CI pipeline
-		new File(targetDir).mkdir();
-		for (String file : sourceFiles){
-			javaToPrettyHtml(file, targetDir);
-		}
+		new File(targetDir).mkdir();	
+		for (String directory : sourceDirectories){
+			collectFileNames( directory );
+		}			
+		createContentList();
+		concatJavaContents();
 		assertTrue (new File(targetDir).listFiles().length >0);
-		 */
 	}
 	
-	private void javaToPrettyHtml( String srcFile, String targetDir) throws IOException
-	{			
-		File srcFileRef = new File(srcFile);
-		FileInputStream input = new FileInputStream(srcFileRef);
-		String newName = srcFileRef.getName().replace(".java", ".html");
-		FileOutputStream output = new FileOutputStream(new File(targetDir+"/"+newName));
-		
-		byte[] buf = new byte[1024];
-		int bytesRead;
-		
-		Writer w = new OutputStreamWriter(output, "UTF-8");
-		w.write(prefix);
-		w.flush();
-		
-		while ((bytesRead = input.read(buf)) > 0) {
-			output.write(buf, 0, bytesRead);
+	private void createContentList(){		
+		String contentEntry = "<a href='#%s'>%s</a><br>";
+		contentHtml.append("<h2>YaaS Bites Source Code</h2>");
+		contentHtml.append("<h3>Content</h3>");
+		for(File f : listOfFiles){
+			if (f.getName().contains(".java")){
+				String fileName = f.toURI().toString().substring( f.toURI().toString().indexOf("../../")+6);
+				String entry = String.format( contentEntry, fileName, fileName);
+				contentHtml.append( entry );
+			}
 		}
+	}
+	
+	private void concatJavaContents( ) throws IOException
+	{			
+		mainHtml.append( pagePrefix );
+		mainHtml.append( contentHtml );;
+		for(File f : listOfFiles){
+			if (f.getName().contains(".java")){
+				String fileName = f.toURI().toString().substring( f.toURI().toString().indexOf("../../")+6);
+				
+				String codePrefixWithContent = String.format( codePrefix, fileName, fileName );
+				mainHtml.append(codePrefixWithContent);			
+				byte[] encoded = Files.readAllBytes(Paths.get(f.toURI()));			  
+				mainHtml.append( new String(encoded, StandardCharsets.UTF_8));
+				mainHtml.append(codePostfix);
+				System.out.println(f.getName());
+			}
+		}		
 		
-		w.write(postfix);
-		w.flush();
-		
-		input.close();
-		output.close();
+		mainHtml.append( pagePostfix );		
+
+		  BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(targetFile /*"/Users/d061192/Desktop/demo.html"*/ )));
+		  bwr.write(mainHtml.toString());
+		  bwr.flush();
+		  bwr.close();     
 	}
 
+	private void collectFileNames(String directoryName) {
+	    File directory = new File(directoryName);
+	    File[] fList = directory.listFiles();
+	    for (File file : fList) {
+	        if (file.isFile()) {
+	        	listOfFiles.add(file);
+	        } else if (file.isDirectory()) {
+	        	collectFileNames(file.getAbsolutePath());
+	        }
+	    }
+	}
+	
 }

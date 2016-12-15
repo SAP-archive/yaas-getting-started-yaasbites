ef6dd810adbdc03143c57e6b804c02283bd3cb87
KenLomax <ken.lomax@sap.com>
2016-12-15 11:10:26 +0100

    refactor prettifier

--- admin/yaasbitesadmin/src/test/java/com/hybris/yaas/bites/PrettifierTest.java
+++ admin/yaasbitesadmin/src/test/java/com/hybris/yaas/bites/PrettifierTest.java
@@ -16,12 +16,12 @@ public class PrettifierTest {	// Using https://github.com/google/code-prettify
 	@Test
 	public void convertGithubSnippetsToHTML() throws Exception {	
 		Map<String, String> snippets = pe.getSnippets( 
-			new URL("https://raw.githubusercontent.com/SAP/yaas-getting-started-yaasbites/master/admin/yaasbitesadmin/src/test/java/com/hybris/yaas/bites/SanityTest.java"));
+			new URL("https://raw.githubusercontent.com/SAP/yaas-getting-started-yaasbites/master/admin/yaasbitesadmin/src/test/java/com/hybris/yaas/bites/SanityTest.java?nocache="+System.currentTimeMillis()));
 		pe.saveSnippets( "src/main/webapp/demotests", snippets);
-		//assertTrue(new File("src/main/webapp/demotests/greetingTest.html").exists());
+		assertTrue(new File("src/main/webapp/demotests/greetingTest.html").exists());
 		
 		snippets = pe.getSnippets( 
-			new File("../../essentials/yaasbite100/src/main/java/com/hybris/yaas/bites/GreetingController.java"));
+			new File("../../essentials/yaasbite100/src/main/java/com/hybris/yaas/bites/GreetingController.java?nocache="+System.currentTimeMillis()));
 		pe.saveSnippets( "src/main/webapp/demotests", snippets);
 		assertTrue(new File("src/main/webapp/demotests/greetingEndpoint.html").exists());
 	}
